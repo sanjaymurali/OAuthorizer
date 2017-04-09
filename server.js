c71e9cf8c0184fa89f32cd816413d064491afdfd
Sanjay Murali <sanjaymurali@ccs.neu.edu>
2017-04-09 17:39:40 -0400

    Changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 var connectionString = 'mongodb://127.0.0.1:27017/webdev-project'; //Local connectionString
 
 if (process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
+    connectionString = "mongodb://"+process.env.MLAB_USERNAME + ":" +
         process.env.MLAB_PASSWORD + "@" +
         process.env.MLAB_HOST + ':' +
         process.env.MLAB_PORT + '/' +
