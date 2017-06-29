cd48d485c33905a05de00fe084df9a7ee06e5eb8
Sanjay Murali <sanjaymurali@ccs.neu.edu>
2017-06-30 01:35:27 +0530

    Changed to Angular 4 - Initial

---
+++ server.config.js
@@ -0,0 +1,50 @@
+/**
+ * Created by sanjaymurali on 11/06/17.
+ */
+
+module.exports = function(app) {
+
+    var expressSession = require('express-session');
+    var cookieParser = require('cookie-parser');
+    var MongoStore = require('connect-mongo')(expressSession);
+    var passport = require('passport');
+    var cors = require('cors');
+
+    app.use(require('express-favicon-short-circuit'));
+
+    app.use(cors());
+    app.use(cookieParser());
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    var connectionString = 'mongodb://127.0.0.1:27017/webdev-project'; //Local connectionString
+
+    if (process.env.MLAB_USERNAME) {
+        connectionString = "mongodb://"+process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+    app.use(expressSession({
+        secret: "bigsecret!",
+        store: new MongoStore({
+            url: connectionString
+        }),
+        resave: true,
+        saveUninitialized: true,
+        autoRemove: 'interval',
+        autoRemoveInterval: 1, // In minutes. Default
+        cookie: {maxAge: 600000}
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+// configure a public directory to host static content
+
+    require("./api/dbconnection/connection")(app);
+
+};
--- server.js
+++ server.js
@@ -1,63 +1,38 @@
 var express = require('express');
-var expressSession = require('express-session');
-var cookieParser = require('cookie-parser');
-var MongoStore = require('connect-mongo')(expressSession);
-var passport = require('passport');
-var cors = require('cors');
 
 var app = express();
 
-app.use(require('express-favicon-short-circuit'));
-
-app.use(cors());
-app.use(cookieParser());
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-var connectionString = 'mongodb://127.0.0.1:27017/webdev-project'; //Local connectionString
-
-if (process.env.MLAB_USERNAME) {
-    connectionString = "mongodb://"+process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
-}
-
-app.use(expressSession({
-    secret: "bigsecret!",
-    store: new MongoStore({
-        url: connectionString
-    }),
-    resave: true,
-    saveUninitialized: true,
-    autoRemove: 'interval',
-    autoRemoveInterval: 1, // In minutes. Default
-    cookie: {maxAge: 600000}
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
-
-require("./api/dbconnection/connection")(app);
-
-var port = process.env.PORT || 3000;
-
+require('./server.config')(app);
 require("./api/app.js")(app);
+
+/* Angular 1.5 */
+
+/*app.use(express.static(__dirname + '/public'));
 
 //Used to setup the client
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/views', express.static(__dirname + '/public/views'));
 
+app.all('/!*', function (req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', {root: 'public'});
+});*/
+
+/* Angular 2+ */
+
+app.use(express.static(__dirname + '/dist'));
+
+//Used to setup the client
+app.use('/js', express.static(__dirname + '/dist/js'));
+app.use('/css', express.static(__dirname + '/dist/css'));
+app.use('/views', express.static(__dirname + '/dist/views'));
 
 app.all('/*', function (req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', {root: 'public'});
+    res.sendFile('index.html', {root: 'dist'});
 });
+
+var port = process.env.PORT || 3000;
 
 app.listen(port);
