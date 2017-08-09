d0f458993c5b3c1f4fabe4450b634d5e09fba4f6
Sanjay Murali <sanjaymurali@ccs.neu.edu>
2017-08-09 12:42:05 +0530

    Changes

--- api/dbconnection/connection.js
+++ api/dbconnection/connection.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     var connectionString = 'mongodb://127.0.0.1:27017/webdev-project'; //Local connectionString
 
     if (process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
+        connectionString = 'mongodb://'+process.env.MLAB_USERNAME + ":" +
             process.env.MLAB_PASSWORD + "@" +
             process.env.MLAB_HOST + ':' +
             process.env.MLAB_PORT + '/' +
